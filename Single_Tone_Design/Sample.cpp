9e5e84e69c5ca0fb8f77f15158694bad3259db0c
mrrajamiaraj <116349287+mrrajamiaraj@users.noreply.github.com>
2025-08-13 22:11:18 +0600

    rmr

---
+++ Single_Tone_Design/Sample.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+using namespace std;
+
+class Singleton{
+private:
+    static Singleton* instance;
+    Singleton(){
+        cout<<"Singleton Constructor called.New object created"<<endl;
+    }
+public:
+    static Singleton* getInstance(){
+        if(instance==nullptr){
+            instance= new Singleton();
+        }
+        return instance;
+    }
+};
+
+Singleton* Singleton::instance =nullptr;
+
+int main(){
+    Singleton* s1 = Singleton::getInstance();
+    Singleton* s2 = Singleton::getInstance();
+
+    cout<<(s1==s2) <<endl;
+
+}
